5615a7c63e45f41acc8c791def8c3f912130c1c9
vhob-dev <dev.vhobtrabalhos@gmail.com>
2024-09-02 20:17:44 -0300

    first commit

---
+++ src/fisrtanotation.java
@@ -0,0 +1,11 @@
+@anotação(value = "tabela", bairros = "konoha", niveldepoder = 10)
+class firstanotation {
+
+    @anotação(value = "tabela", bairros = {"konoha", "viladafolha"}, niveldepoder= 20, valores = 100d)
+    private String nome;
+
+    @anotação(value = "tabela", bairros = "konoha", niveldepoder = 10)
+    public firstanotation() {
+
+    }
+}
